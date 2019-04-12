6b3440cb3adecb58d637df6c53b345b143ff82cc
Florian Aubin <flobin@localhost.localdomain>
2019-04-12 16:06:21 +0200

    base airport.php

---
+++ airport.php
@@ -0,0 +1,22 @@
+<?php
+
+function text_display($str)
+{
+	$maxletters = strlen($str);
+	for($i=0; $i < $maxletters + 1; $i++)
+	{
+		if ($i == $maxletters)
+		{
+			$i = 0;
+			usleep(200000);
+		}
+		else
+		{
+			@system('clear');
+			echo substr($str . $str, $i, 30);
+			usleep(200000);
+		}
+	}
+}
+
+?>
