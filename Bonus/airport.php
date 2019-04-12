3afe540c920c703a855b466030b5674b28717ece
Florian Aubin <flobin@localhost.localdomain>
2019-04-12 16:49:14 +0200

    set movespeed at half a second

--- Bonus/airport.php
+++ Bonus/airport.php
@@ -8,18 +8,18 @@ function text_display_color($str, $len)
 		if ($i == $totallen)
 		{
 			$i = 0;
-			usleep(200000);
+			usleep(500000);
 		}
 		else
 		{
 			@system('clear');
 			echo substr($str . $str, $i, $len);
-			usleep(200000);
+			usleep(500000);
 		}
 	}
 	shell_exec("tput sgr0");
 }
 
-text_display_color("C'est vous qui êtes enfermé ici avec moi !", 50);
+text_display_color("Je ne fais que mettre en exergue le paradoxe qui est de demander à un homme masqué qui il est.", 35);
 
 ?>
